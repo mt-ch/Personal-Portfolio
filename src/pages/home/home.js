f2dcf32eaa24a52d359464bcc77a82a5f3482713
mt-ch <matthew.chan@icloud.com>
2020-10-19 22:13:38 +0100

    contact + home page

---
+++ src/pages/home/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+//Components
+import Landing from "../../components/landing/landing";
+import FeaturedWork from "../../components/featuredWork/featuredWork";
+
+const Home = () => {
+  return (
+    <div>
+      <Landing />
+      <FeaturedWork />
+    </div>
+  );
+};
+
+export default Home;
