53104b1d444f46816c79330722d8b3ca00e97b2f
mt-ch <matthew.chan@icloud.com>
2020-10-21 23:06:23 +0100

    Animated gradient background

---
+++ src/components/shapes.js
@@ -0,0 +1,52 @@
+import React, { Children } from "react";
+import styled from "styled-components";
+
+export const StyledShapes = styled.div`
+  height: 60vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: solid black 1px;
+
+  background: linear-gradient(
+    143deg,
+    #ff4242,
+    #faa018,
+    #56be3b,
+    #c496ff,
+    #f153ef
+  );
+  background-size: 1000% 1000%;
+
+  -webkit-animation: AnimationName 16s ease infinite;
+  -moz-animation: AnimationName 16s ease infinite;
+  animation: AnimationName 16s ease infinite;
+
+  @keyframes AnimationName {
+    0% {
+      background-position: 7% 0%;
+    }
+    50% {
+      background-position: 94% 100%;
+    }
+    100% {
+      background-position: 7% 0%;
+    }
+  }
+
+  .page-title {
+    color: white;
+    font-weight: bold;
+    font-size: 5rem;
+  }
+`;
+
+const Shapes = props => {
+  return (
+    <StyledShapes>
+      <p className="page-title">{props.title}</p>
+    </StyledShapes>
+  );
+};
+
+export default Shapes;
