99b155ae3fcb9c951d0c504cdf29654e5a6caa41
mt-ch <matthew.chan@icloud.com>
2020-10-25 23:28:40 +0000

    Footer restyled

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import Arrow from "../../assets/icons/arrow";
 import { StyledFooter } from "./footer.styled";
 
 const Footer = () => {
   return (
     <StyledFooter>
-      <ul>
+      {/* <ul>
         <li>
           <Link to="/projects">
             <p>Projects</p>
@@ -19,9 +19,20 @@ const Footer = () => {
             <Arrow height={"20"} width={"20"} fill={"black"} />
           </Link>
         </li>
-      </ul>
+      </ul> */}
+      <p className='footer-text'>Let's get in touch!</p>
 
-      <p>© 2020 Matthew Chan</p>
+      <p className='footer-email'>me@matt.chan</p>
+
+      <div className="footer-social">
+        <div className="footer-social-div">
+          <p className='footer-social-text'>Socials</p>
+          <Arrow height={"20"} width={"20"} fill={"hsla(0,0%,100%,.2)"} />
+        </div>
+        <p className='footer-social-text'>Github · LinkedIn</p>
+      </div>
+
+      <p className='footer-cr'>© 2020 Matthew Chan</p>
     </StyledFooter>
   );
 };
--- src/components/footer/footer.styled.js
+++ src/components/footer/footer.styled.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 export const StyledFooter = styled.div`
-  padding: 1em;
+  /* border-top: black solid 1px; */
 
-  ul {
+  /* ul {
     margin-bottom: 1em;
     a {
       display: flex;
@@ -11,14 +11,53 @@ export const StyledFooter = styled.div`
       justify-content: start;
       width: 100px;
     }
+  } */
+
+  background-color: #212121;
+  /* padding: 1em; */
+  .footer-text{
+    padding: 3.5rem 2rem 0rem 2rem;
+    font-size: 4rem;
+    font-weight: bold;
+    color: #fcec62;
+  }
+
+  .footer-email{
+    padding: 4rem 2rem 4rem 2rem;
+    font-size: 3rem;
+    text-decoration: underline;
+  }
+
+  .footer-social {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: solid hsla(0, 0%, 100%, 0.2) 1px;
+    padding: 1rem 2rem 1rem 2rem;
+
+    .footer-social-div {
+      display: flex;
+      align-items: center;
+    }
+
+    .footer-social-text {
+      color: hsla(0, 0%, 100%, 0.2);
+    }
+  }
+
+  .footer-cr {
+    color: hsla(0, 0%, 100%, 0.2);
+    padding: 1rem 2rem 1rem 2rem;
   }
 
   p {
-    margin-right: 5px;
+    /* margin-right: 5px; */
     font-size: 1.5rem;
+    color: white;
   }
 
-  svg{
+  svg {
     flex-shrink: 0;
+    margin-left: 5px;
   }
 `;
