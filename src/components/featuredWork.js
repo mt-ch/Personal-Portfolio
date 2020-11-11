59ddd670076f09948df5b055c3a7e46d46f042fc
mt-ch <matthew.chan@icloud.com>
2020-11-11 13:39:05 +0000

    New Headers

--- src/components/featuredWork.js
+++ src/components/featuredWork.js
@@ -1,15 +1,52 @@
-import React, { useEffect, useState } from "react"
-import GetFeatured from "../functions/getFeaturedProjects"
-import Accordion from './accordion'
+import React, { useEffect, useState } from "react";
+import GetFeatured from "../functions/getFeaturedProjects";
+import Accordion from "./accordion";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import styled from "styled-components";
+
+const StyledFeatured = styled.div`
+  .para {
+    margin-top: 13vw;
+
+    h1{
+        font-size: 10vw;
+    }
+
+    h2{
+        font-size: 9vw;
+        color: white;
+    }
+
+    p{
+        font-size: 6vw;
+        line-height: 1.3;
+    }
+  }
+`;
 
 const FeaturedWork = () => {
-  const [featured, setFeatured] = useState([])
+  const [featured, setFeatured] = useState([]);
   useEffect(() => {
-    GetFeatured(setFeatured)
-  }, [])
+    GetFeatured(setFeatured);
+    AOS.init();
+  }, []);
   return (
-    <div>
-      {featured.map(project => (
+    <StyledFeatured>
+      <div
+        class="para"
+        data-aos="fade-up"
+        data-aos-offset="250"
+        data-aos-delay="100"
+        data-aos-duration="800"
+        data-aos-easing="ease"
+      >
+        <h1 className="section-title">
+          {" "}
+          <small> (002) </small> MY WORK
+        </h1>
+      </div>
+      {/* {featured.map((project) => (
         <Accordion
           title={project.name}
           roles={project.roles}
@@ -19,13 +56,13 @@ const FeaturedWork = () => {
           website={project.website}
           coverPhoto={project.coverPhoto}
           photoAlbum={project.photos}
-          text={'white'}
-          openText={'black'}
+          text={"white"}
+          openText={"black"}
           link={project.id}
         />
-      ))}
-    </div>
-  )
-}
+      ))} */}
+    </StyledFeatured>
+  );
+};
 
-export default FeaturedWork
+export default FeaturedWork;
